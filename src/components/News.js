09813026d05f5759e9c04132634d8f92d3b6054e
fizaayesha <fizaayesha696@gmail.com>
2022-06-29 14:45:32 +0530

    first commit

--- src/components/News.js
+++ src/components/News.js
@@ -110,7 +110,7 @@ export class News extends Component {
         {this.state.loading && <Spinner />}
         <div className="row">
           {!this.state.loading &&
-            this.state.articles.map((element) => {
+            this.state.articles?.map((element) => {
               return (
                 <div className="col-md-4" key={element.url}>
                   <NewsItem
